e2f494cbad8df0a83b725a46d6a083f0d28297f0
mihaimiron1 <mihaimiron004@gmail.com>
2025-12-03 10:19:11 +0200

    add rezultate

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace MyApp.Controllers
             return View();
         }
 
+        public IActionResult Rezultate()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
