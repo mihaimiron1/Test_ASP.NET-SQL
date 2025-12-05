ac11e4e060495ede24eba31450ea6079de797799
mihaimiron1 <mihai@MIHAI>
2025-12-05 17:26:21 +0200

    test

--- MyApp/wwwroot/js/site.js
+++ MyApp/wwwroot/js/site.js
@@ -2,4 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-import ApexCharts from 'apexcharts';
+import ApexCharts from 'apexcharts'; 
